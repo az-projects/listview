243984956f479d60e10ec95b0bac4ca1f2aff071
willard-yuan <yongyuanstu@gmail.com>
2014-05-07 23:27:15 +0800

    Update urls.py

--- 1/readlist/urls.py
+++ 1/readlist/urls.py
@@ -1,9 +1,11 @@
-
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 from models import *
 from views import *
+from django.contrib import admin
+
  
 
 urlpatterns = patterns('',
+  url(r'^admin/',include(admin.site.urls)),
   url(r'^hello/$',hello),
 )
